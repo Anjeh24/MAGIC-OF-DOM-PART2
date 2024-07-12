505c1b9f464c312893405254cf72eaa00e69970a
anjeh24 <junez24@yahoo.com>
2024-07-11 20:07:47 -0400

    iinitial commit

---
+++ scripts/script.js
@@ -0,0 +1,71 @@
+//import "../styles/style.css";
+//Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+  
+  //Select and cache the <main> element in a variable named mainEl.
+  
+  let mainEl = document.querySelector('main');
+  
+  //Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+  //Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+  
+  //let root = document.querySelector('.root');
+  mainEl.style.backgroundColor = 'var(--main-bg)';
+  
+  //mainEl.style.backgroundColor = 'var(--main-bg)';
+  
+  //Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+  
+  const head1 = document.createElement('h1');
+  head1.textContent = "DOM Manipulation";
+  mainEl.appendChild(head1);
+  
+  //Add a class of flex-ctr to mainEl.
+  //Hint: Use the Element.classList API.
+  
+  mainEl.classList = "flex-ctr";
+  
+  //Part 2: Creating a Menu Bar
+  //Next, create a blank menu bar that we can use to later add some interactivity to the page:
+  
+  //1. Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+  let topMenuEl = document.getElementById("top-menu");
+  topMenuEl.style.height = "100%"
+  topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+  topMenuEl.classList = "flex-around";
+  
+  
+  // Part 3: Adding Menu Buttons
+  // Very often, you will be working with data provided by external sources in a variety of ways.
+  //  For this project, copy the following data structure to the top of your index.js file; you will use it to create your menu buttons.
+  
+  
+   for (let a of menuLinks){
+   
+    let headerLinks = document.createElement('a');
+  
+     headerLinks.setAttribute('href', a.href);
+    headerLinks.textContent  = a.text;
+     topMenuEl.appendChild(headerLinks);
+    
+  
+    
+   }
+  
+  
+  
+  
+  
+  
+  
+  //new link element
+  
+  //const attri = document.createAttribute('href');
+   
+  // let ar = menuLinks[0].href
+  // console.log(ar)
